91a61e100539c8b85cfb3f5f6410dec75ae097ee
prashant-software-developer <prashantkumarsingh.work@gmail.com>
2026-02-08 17:23:28 +0530

    fix: update grid layout for responsiveness in ChatbotPage

--- app/dashboard/chatbot/page.tsx
+++ app/dashboard/chatbot/page.tsx
@@ -129,8 +129,8 @@ const ChatbotPage = () => {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 flex-1 min-h-0">
-        <div className="lg:col-span-5 h-full min-h-0 overflow-hidden flex flex-col">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-4 flex-1 min-h-0 w-full">
+        <div className="md:col-span-5 h-full min-h-0 overflow-hidden flex flex-col">
           <ScrollArea className="h-full">
             <div className="space-y-4 pr-4 pb-4">
               <ApperanceConfig
@@ -147,7 +147,7 @@ const ChatbotPage = () => {
             </div>
           </ScrollArea>
         </div>
-        <div className="lg:col-span-7 flex flex-col h-full min-h-0">
+        <div className="md:col-span-7 flex flex-col h-full min-h-0">
           <ChatSimulator
             messages={messages}
             primaryColor={primaryColor}
